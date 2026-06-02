ba39a9844a7c0f23a959f22d06b2c954a3df8ed5
averyocean65 <averytoad65@gmail.com>
2026-06-02 15:03:47 +0200

    player looking works!

--- Starventure/Assets/Scripts/Player/PlayerLook.cs
+++ Starventure/Assets/Scripts/Player/PlayerLook.cs
@@ -3,16 +3,31 @@ using UnityEngine;
 
 namespace Starventure.Player {
     public class PlayerLook : MonoBehaviour {
+        private const float SensitivityMultiplier = 0.1f;
+        
         [SerializeField] private Transform orientation;
         [SerializeField] private new Transform camera;
-        [SerializeField] private Vector2 lookSensitivity;
+        [SerializeField] private Vector2 lookSensitivity = Vector2.one;
+        
+        [Tooltip("X represent the upper bound, Y represents the lower bound.")]
+        [SerializeField] private Vector2 viewClamp = new Vector2(-90, 90);
 
         private Vector2 _lookDelta;
+        private float _xRotation;
+
+        private void Start() {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
 
         private void Update() {
-            _lookDelta = InputManager.Player.Look.ReadValue<Vector2>() * lookSensitivity;
-            orientation.Rotate(0, _lookDelta.x, 0, Space.Self);
-            camera.Rotate(_lookDelta.y, _lookDelta.x, 0, Space.Self);
+            _lookDelta = InputManager.Player.Look.ReadValue<Vector2>() * lookSensitivity * SensitivityMultiplier;
+            camera.Rotate(0, _lookDelta.x, 0, Space.Self);
+
+            _xRotation += _lookDelta.y;
+            _xRotation = Mathf.Clamp(_xRotation, viewClamp.x, viewClamp.y);
+            
+            camera.localEulerAngles = new Vector3(-_xRotation, camera.localEulerAngles.y, 0);
+            orientation.localEulerAngles = new Vector3(0, camera.localEulerAngles.y, 0);
         }
     }
 }
