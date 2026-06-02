4e0132278986b63838b7c120ddb665d71b4360c7
averyocean65 <averytoad65@gmail.com>
2026-06-02 11:28:26 +0200

    begin work on looking

--- Starventure/Assets/Scripts/Player/PlayerLook.cs
+++ Starventure/Assets/Scripts/Player/PlayerLook.cs
@@ -1,8 +1,18 @@
+using System;
 using UnityEngine;
 
 namespace Starventure.Player {
     public class PlayerLook : MonoBehaviour {
         [SerializeField] private Transform orientation;
         [SerializeField] private new Transform camera;
+        [SerializeField] private Vector2 lookSensitivity;
+
+        private Vector2 _lookDelta;
+
+        private void Update() {
+            _lookDelta = InputManager.Player.Look.ReadValue<Vector2>() * lookSensitivity;
+            orientation.Rotate(0, _lookDelta.x, 0, Space.Self);
+            camera.Rotate(_lookDelta.y, _lookDelta.x, 0, Space.Self);
+        }
     }
 }
