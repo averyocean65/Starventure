c1651fb3dfe1bd0799ce4e492edb258b0e61e1a5
averyocean65 <averytoad65@gmail.com>
2026-06-03 22:04:21 +0200

    orbiting works better now

--- Starventure/Assets/Scripts/Physics/Orbitor.cs
+++ Starventure/Assets/Scripts/Physics/Orbitor.cs
@@ -7,35 +7,42 @@ using UnityEditor;
 #endif
 
 namespace Starventure.Physics {
-    public class Orbitor : MonoBehaviour {
-        private const float TwoPI = 6.2831853072f; 
+    public class Orbitor : MonoBehaviour { 
+        private const float SpeedScale = 0.01f; 
         
         [SerializeField] private Rigidbody rb;
         
         [SerializeField] private float speed = 1.0f;
+        public float startOffset = 0.0f;
         public Transform orbitCenter;
         public Vector2 ellipseSize = Vector2.one;
-        private Vector2 scaledEllipseSize;
+        private Vector2 _scaledEllipseSize;
 
         private float _theta;
-        private float PeriodLength => TwoPI / speed;
 
         private void Start() {
             transform.parent = orbitCenter;
-            scaledEllipseSize = new Vector2(ellipseSize.x / orbitCenter.lossyScale.x,
+            _scaledEllipseSize = new Vector2(ellipseSize.x / orbitCenter.lossyScale.x,
                 ellipseSize.y / orbitCenter.lossyScale.z);
+
+            _theta = startOffset;
+        }
+
+        public Vector3 SampleRawPosition(float time) {
+            return SampledScaledPosition(time, Vector2.one);
+        }
+
+        public Vector3 SampledScaledPosition(float time, Vector2 scale) {
+            // (a*cos(x)+b, 0, c*sin(x)+d)
+            return new Vector3(scale.x * Mathf.Cos(time),
+                0,
+                scale.y * Mathf.Sin(time)
+            );
         }
 
         private void Update() {
-            _theta += speed * Time.deltaTime;
-            _theta %= PeriodLength;
-            
-            // (a*cos(x)+b, c*sin(x)+d)
-            Vector3 sampled = new Vector3(scaledEllipseSize.x * Mathf.Cos(_theta),
-                0,
-                scaledEllipseSize.y * Mathf.Sin(_theta));
-
-            transform.localPosition = sampled;
+            _theta += speed * SpeedScale * Time.deltaTime;
+            transform.localPosition = SampledScaledPosition(_theta, _scaledEllipseSize);
         }
 
         private void OnEnable() {
@@ -69,8 +76,11 @@ namespace Starventure.Editor {
             
             Handles.color = Color.yellow;
             Handles.matrix = Matrix4x4.TRS(t.orbitCenter.position, t.orbitCenter.rotation,
-                new Vector3(t.ellipseSize.x, 0, t.ellipseSize.y));
+                new Vector3(t.ellipseSize.x, 1, t.ellipseSize.y));
             Handles.DrawWireDisc(Vector3.zero, Vector3.up, 1.0f);
+
+            Handles.matrix = Matrix4x4.TRS(Vector3.zero, t.orbitCenter.rotation, Vector3.one);
+            Handles.DrawWireCube(t.SampledScaledPosition(t.startOffset, t.ellipseSize) + t.orbitCenter.position, Vector3.one);
         }
     }
 }
