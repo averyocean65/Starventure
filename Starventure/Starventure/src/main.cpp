d96aafce806e3cc190392b2052330248d1fcb9cc
averyocean65 <averytoad65@gmail.com>
2026-06-12 12:19:29 +0200

    try adding raylib

--- Starventure/Starventure/src/main.cpp
+++ Starventure/Starventure/src/main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <raylib.h>
 
 int main(int argc, char** argv) {
-	std::cout << "Hello World" << std::endl;
+	int height = 960;
+	int width = 540;
+
+	InitWindow(width, height, "Starventure");
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+
+		ClearBackground(BLACK);
+		DrawText("Starventure will now be coming to Raylib and C++!", width/2, height/2, 12, WHITE);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+
 	return 0;
 }
